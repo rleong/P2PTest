f401a5184638e5fd2f71efd7bbca9e21ac374bae
Russell <Russell@rleong.home>
2018-06-18 01:17:39 -0400

    Finally realized what I was doing wrong, I was throwing errors

--- src/client/client.java
+++ src/client/client.java
@@ -15,36 +15,41 @@ import javax.swing.JOptionPane;
 
 public class client {
 	
-	public static void main(String[] args) throws UnknownHostException, IOException {
+	public static void main(String[] args) {
 		
-		ServerSocket listener = new ServerSocket(9090);
-		String serverAddress = JOptionPane.showInputDialog(
-	            "Enter IP Address of a machine that is\n" +
-	            "running the date service on port 9090:");
-		Socket s = new Socket(serverAddress, 9090);
 		try {
-			while (true) {
-				Socket socket = listener.accept();
-				try {
-					PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-					out.println(new Date().toString());
-				} finally {
-					socket.close();
+			ServerSocket listener = new ServerSocket(9090);
+			String serverAddress = JOptionPane.showInputDialog(
+		            "Enter IP Address of a machine that is\n" +
+		            "running the date service on port 9090:");
+			Socket s = new Socket(serverAddress, 9090);
+			try {
+				while (true) {
+					Socket socket = listener.accept();
+					try {
+						PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+						out.println(new Date().toString());
+					} finally {
+						socket.close();
+					}
+					try {
+						BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
+				        String answer = input.readLine();
+				        JOptionPane.showMessageDialog(null, answer);
+					} catch (SocketException e) {
+						JOptionPane.showMessageDialog(null, e);
+					} catch (IOException e2) {
+						JOptionPane.showMessageDialog(null, e2);
+					}
+					finally {
+						System.exit(0);
+					}		        
 				}
-				try {
-					BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
-			        String answer = input.readLine();
-			        JOptionPane.showMessageDialog(null, answer);
-				} catch (SocketException e) {
-					e.printStackTrace();
-					JOptionPane.showMessageDialog(null, "Network is unreachable!\nTry again.");
-				}
-				finally {
-					System.exit(0);
-				}		        
+			} finally {
+				listener.close();
 			}
-		} finally {
-			listener.close();
+		} catch (IOException e1) {
+			JOptionPane.showMessageDialog(null, e1);
 		}
 
 		
